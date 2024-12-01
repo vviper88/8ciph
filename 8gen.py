53b8b9092a769d47f6f258e4993f2b8d9c2ed5fe
vviper88 <linaresest778@protonmail.com>
2024-12-01 23:14:34 +0000

    Multiple substitutes for most characters.

--- 8gen.py
+++ 8gen.py
@@ -3,7 +3,7 @@ import secrets
 
 min_num = 10000000
 max_num = 99999999
-LBL_CHARS = tuple("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!?.,:][/&%@`'~+-_ ")
+LBL_CHARS = tuple("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=")
 used_crypts = []
 
 print("-- BEGIN PAD --")
@@ -11,8 +11,12 @@ for i in LBL_CHARS:
     while True:
         ciph = str(secrets.randbelow(max_num - min_num + 1) + min_num)
         if ciph not in used_crypts and "666" not in ciph:
-            break
-    print(f"{i}: {ciph}")
+            ciph2 = str(secrets.randbelow(max_num - min_num + 1) + min_num)
+            if ciph2 not in used_crypts and "666" not in ciph:
+                ciph3 = str(secrets.randbelow(max_num - min_num + 1) + min_num)
+                if ciph2 not in used_crypts and "666" not in ciph:
+                    break
+    print(f"{i}: {ciph} {ciph2} {ciph3}")
 while True:
     ciph = str(secrets.randbelow(max_num - min_num + 1) + min_num)
     if ciph not in used_crypts and "666" not in ciph:
