9d79a3b81e0246be4641e840ec03f2d26889c105
vviper88 <linaresest778@protonmail.com>
2024-12-03 18:20:24 +0000

    Update 8gen.py

--- 8gen.py
+++ 8gen.py
@@ -11,15 +11,25 @@ for i in LBL_CHARS:
     while True:
         ciph = str(secrets.randbelow(max_num - min_num + 1) + min_num)
         if ciph not in used_crypts and "666" not in ciph:
+            used_crypts.append(ciph)
             ciph2 = str(secrets.randbelow(max_num - min_num + 1) + min_num)
-            if ciph2 not in used_crypts and "666" not in ciph:
+            if ciph2 not in used_crypts and "666" not in ciph2:
+                used_crypts.append(ciph2)
                 ciph3 = str(secrets.randbelow(max_num - min_num + 1) + min_num)
-                if ciph2 not in used_crypts and "666" not in ciph:
+                if ciph3 not in used_crypts and "666" not in ciph3:
+                    used_crypts.append(ciph3)
                     break
     print(f"{i}: {ciph} {ciph2} {ciph3}")
 while True:
     ciph = str(secrets.randbelow(max_num - min_num + 1) + min_num)
     if ciph not in used_crypts and "666" not in ciph:
-        break
-print(f"RLL: {ciph}")
+        used_crypts.append(ciph)
+        ciph2 = str(secrets.randbelow(max_num - min_num + 1) + min_num)
+        if ciph2 not in used_crypts and "666" not in ciph2:
+            used_crypts.append(ciph2)
+            ciph3 = str(secrets.randbelow(max_num - min_num + 1) + min_num)
+            if ciph3 not in used_crypts and "666" not in ciph3:
+                used_crypts.append(ciph3)
+                break
+print(f"RLL: {ciph} {ciph2} {ciph3}")
 print("-- END PAD --")
